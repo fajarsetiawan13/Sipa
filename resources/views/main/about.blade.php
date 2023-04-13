e0bdd0922fe17419c4cd761beed29d70c9229416
fajarsetiawan13 <fajar_setiawan13@icloud.com>
2023-04-14 02:37:22 +0700

    add about description

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -2,18 +2,25 @@
 
 @section('section')
 
-<section id="login-page" class="py-8 bg-slate-800">
-    <div class="container py-16 px-4 mx-auto">
-        <div class="w-full">
-            <div class="max-w-xl mx-auto text-center">    
-                <h4 class="font-semibold uppercase text-primary text-sm lg:text-lg">Tentang</h4>
-                <h2 class="font-bold text-slate-50 text-2xl mb-3 lg:text-3xl">{{ $title }}</h2>
-                {{-- <p class="font-medium text-md text-slate-800 md:text-lg mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores nesciunt debitis harum? Delectus possimus a maiores quia accusamus?</p> --}}
+<section id="hero" style="margin-top: 64px;">
+    <div class="hero min-h-screen" style="background-image: url({{ asset('/img/te-unnes.jpg') }});">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content flex-col lg:flex-row lg:px-6">
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body items-center">
+                    <img src="{{ asset('/img/fajar-profile.jpg') }}" class="max-w-sm rounded-lg shadow-2xl" />
+                </div>
             </div>
-        </div>
-        <div class="flex justify-center">
-            <div class="card flex w-[600px] shadow-lg bg-slate-50">
-                <div class="card-body">
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body items-start">
+                    <h2 class="card-title text-slate-800 text-start">Fajar Setiawan</h2>
+                    <h2 class="card-title text-slate-800 text-start">5302416023</h2>
+                    <p class="text-slate-800 text-start">Mahasiswa S-1</p>
+                    <p class="text-slate-800 text-start">Prodi Pendidikan Teknik Informatika dan Komputer</p>
+                    <p class="text-slate-800 text-start">Universitas Negeri Semarang</p>
+                    <div class="divider my-0"></div> 
+                    <p class="card-title text-slate-800 text-start">Pembimbing</p>
+                    <p class="text-slate-800 text-start">Dr. H. Noor Hudallah, M.T.</p>
                 </div>
             </div>
         </div>
